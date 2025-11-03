19aab9bafd0da81e22e99246528641e76e01d3b3
agungferdi <muhammad_agungf@student.ub.ac.id>
2025-11-03 21:51:00 +0700

    refactor: remove C61 RFID Architecture component and associated styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import IntegrationFlowchart from './IntegrationFlowchart.jsx'
 import RetailFlowMindmap from './RetailFlowMindmap.jsx'
-import C61RFIDArchitecture from './C61RFIDArchitecture.jsx'
 import ColdStorageFoodFlow from './ColdStorageFoodFlow.jsx'
 import './index.css'
 
